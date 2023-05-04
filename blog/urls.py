3a0a3c8b139981209de6d9d740d92c012cd677ed
Olly <bruja.meredith@pm.me>
2023-05-04 09:35:52 -0700

    hijacking repo 😎

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "blog"
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('entry/<int:pk>/', views.EntryView.as_view(), name='entry'),
-]
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd build/
+python -m http.server
