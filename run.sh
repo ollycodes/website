e87997f2fd522642a7541e9c1f05b0c2d7265c56
Olly <bruja.meredith@pm.me>
2023-05-11 13:52:18 -0700

    feature: added automated file listening and static site generation on file edits.

--- run.sh
+++ run.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
+contentdir="../content"
+staticdir="../static"
+templatedir="../templates"
+
+cleanup() {
+    echo "Stopping the server..."
+    kill $(pgrep -f "python -m http.server")
+    exit
+}
+trap cleanup SIGINT
+
+python generate.py
 cd build/
-python -m http.server
+python -m http.server &
+inotifywait -r -m -e modify "${contentdir}" "${staticdir}" "${templatedir}" |
+while read -r directory event file; do
+    echo "File '${file}' modified in '${directory}'"
+    kill $(pgrep -f "python -m http.server")
+    cd ..
+    python generate.py
+    cd build/
+    python -m http.server &
+done
